624d6637d8db48b5f55e5e2a9fe190a4bdde621a
Trias Nur Rahman <contact@triasrahman.com>
2017-04-25 07:58:44 +0700

    Implement filter and paginator on Products controller

--- controllers/Products.php
+++ controllers/Products.php
@@ -2,18 +2,48 @@
 
 use Input;
 use Event;
+use League\Fractal\Resource\Collection;
 use Octobro\API\Controllers\ApiController;
 use Octommerce\Octommerce\Models\Product;
 use Octommerce\API\Transformers\ProductTransformer;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class Products extends ApiController
 {
 
     public function index()
     {
-        $products = Product::get();
+        $query = Product::published();
 
-        return $this->respondwithCollection($products, new ProductTransformer);
+        $filter = Input::get('filter');
+
+        if ($filter) {
+
+            // by category
+            if (isset($filter['category']) && $filter['category']) {
+                $categoryIds = explode(',', $filter['category']);
+
+                foreach($categoryIds as $categoryId) {
+                    $query->whereHas('categories', function($query) use ($categoryId) {
+                        $query->whereId($categoryId);
+                    });
+                }
+            }
+
+            // by brand
+            if (isset($filter['brand']) && $filter['brand']) {
+                $brandId = $filter['brand'];
+
+                $query->whereHas('brand', function($query) use ($brandId) {
+                    $query->whereId($brandId);
+                });
+            }
+        }
+
+        // Paginate
+        $paginator = $query->paginate(Input::get('number', 20));
+
+        return $this->respondWithPaginator($paginator, new ProductTransformer);
     }
 
     public function show($id)
