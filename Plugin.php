e93963ad08cef91cb7a5dd69d0f8327f7353be9c
Trias Nur Rahman <contact@triasrahman.com>
2017-05-04 21:57:25 +0700

    Extend includes from UserTransformer

--- Plugin.php
+++ Plugin.php
@@ -1,16 +1,25 @@
 <?php namespace Octommerce\API;
 
 use System\Classes\PluginBase;
+use RainLab\User\Models\User;
+use Octobro\API\Transformers\UserTransformer;
+use Octommerce\API\Transformers\OrderTransformer;
 
 class Plugin extends PluginBase
 {
 	public $require = ['Octobro.API'];
 
-    public function registerComponents()
+    public function boot()
     {
-    }
+    	UserTransformer::extend(function($transformer) {
 
-    public function registerSettings()
-    {
+    		$transformer->availableIncludes[] = 'orders';
+
+    		$transformer->addDynamicMethod('includeOrders', function(User $user) use ($transformer) {
+    			return $transformer->collection($user->orders, new OrderTransformer);
+    		});
+
+    	});
+
     }
 }
