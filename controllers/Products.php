cee818f4b2f2ae337624b3f0ed0150ea30a526e7
adeerwin <adeerwin14@gmail.com>
2017-06-21 14:24:36 +0700

    sort order, hide out of stock

--- controllers/Products.php
+++ controllers/Products.php
@@ -13,6 +13,7 @@ class Products extends ApiController
         $query = Product::published();
 
         $filter = Input::get('filter');
+        $sortOrder = Input::get('sort');
 
 
 
@@ -37,8 +38,59 @@ class Products extends ApiController
                     $query->whereId($brandId);
                 });
             }
+            // by Productlist
+            if (isset($filter['list']) && $filter['list']) {
+                $productListIds = explode(',',$filter['list']);
+                foreach($productListIds as $productListId) {
+                    $query->whereHas('lists', function($query) use ($productListId) {
+                        $query->whereId($productListId);
+                    });
+                }
+            }
+            // by search
+            if (isset($filter['search']) && $filter['search']) {
+                $searchQuery = $filter['search'];
+
+                $query->search($searchQuery);
+            }
+            // by stock
+            if (isset($filter['outstock']) && $filter['outstock']) {
+                $stock = $filter['outstock'];
+                if ($stock == 'true') {
+                    $query->available();
+                }
+                
+            }
         }
 
+        if($sortOrder)
+        {
+         if (in_array($sortOrder, array_keys(Product::$allowedSortingOptions))) {
+            $sortOrderArray = explode(" ", $sortOrder);
+                if ($sortOrder == 'random') {
+                    $query->orderByRaw("RAND()");
+                } elseif ($sortOrderArray[0] == 'sales') {
+
+                    // order by product sold qty for the last 30 days
+                    $query->join(DB::raw("
+                        (
+                            select product_id, sum(qty) as sold
+                            from octommerce_octommerce_order_product
+                            where order_id in
+                            (
+                                select id from octommerce_octommerce_orders
+                                where DATEDIFF(NOW(), created_at) <= 30
+                                and status_code NOT IN (\"expired\", \"waiting\")
+                            )
+                            group by product_id order by sold ". $sortOrderArray[1] ."
+                        ) op
+                        "), 'octommerce_octommerce_products.id', '=', 'op.product_id');
+
+                } else {
+                    $query->orderBy($sortOrderArray[0], $sortOrderArray[1]);
+                }
+            }
+        }
         // Paginate
         $paginator = $query->paginate(Input::get('number', 20));
 
